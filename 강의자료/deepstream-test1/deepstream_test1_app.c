053209a114627702d782ee14d09d2e17d892545b
gapple95 <gapple95@naver.com>
2025-01-08 11:14:12 +0900

    코드수정: test1

--- 강의자료/deepstream-test1/deepstream_test1_app.c
+++ 강의자료/deepstream-test1/deepstream_test1_app.c
@@ -1,8 +1,6 @@
-
-
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2018-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- * SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+ * Copyright (c) 2018-2024 NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
  *
  * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
  * property and proprietary rights in and to this material, related
@@ -30,8 +28,8 @@
 #define MUXER_OUTPUT_WIDTH 1920
 #define MUXER_OUTPUT_HEIGHT 1080
 
-/* Muxer batch formation timeout, for e.g. 40 millisec. Should ideally be set
- * based on the fastest source's framerate. */
+/* Muxer batch formation timeout, for e.g. 40 millisec. 
+ * Should ideally be set based on the fastest source's framerate. */
 #define MUXER_BATCH_TIMEOUT_USEC 40000
 
 /* Check for parsing error. */
@@ -43,36 +41,27 @@
   }
 
 gint frame_number = 0;
-gchar pgie_classes_str[4][32] = {"Vehicle", "TwoWheeler", "Person",
-                                 "Roadsign"};
+gchar pgie_classes_str[4][32] = {"Vehicle", "TwoWheeler", "Person", "Roadsign"};
 
-/* osd_sink_pad_buffer_probe  will extract metadata received on OSD sink pad
- * and update params for drawing rectangle, object information etc. */
-
+/* Probe function to parse metadata and print object counts */
 static GstPadProbeReturn
-osd_sink_pad_buffer_probe(GstPad *pad, GstPadProbeInfo *info,
-                          gpointer u_data)
+osd_sink_pad_buffer_probe(GstPad *pad, GstPadProbeInfo *info, gpointer u_data)
 {
   GstBuffer *buf = (GstBuffer *)info->data;
-  guint num_rects = 0;
-  NvDsObjectMeta *obj_meta = NULL;
-  guint vehicle_count = 0;
-  guint person_count = 0;
-  NvDsMetaList *l_frame = NULL;
-  NvDsMetaList *l_obj = NULL;
-  NvDsDisplayMeta *display_meta = NULL;
-
   NvDsBatchMeta *batch_meta = gst_buffer_get_nvds_batch_meta(buf);
 
-  for (l_frame = batch_meta->frame_meta_list; l_frame != NULL;
-       l_frame = l_frame->next)
+  NvDsMetaList *l_frame = NULL;
+  NvDsMetaList *l_obj = NULL;
+  guint num_rects = 0;
+  guint vehicle_count = 0;
+  guint person_count = 0;
+
+  for (l_frame = batch_meta->frame_meta_list; l_frame != NULL; l_frame = l_frame->next)
   {
     NvDsFrameMeta *frame_meta = (NvDsFrameMeta *)(l_frame->data);
-    int offset = 0;
-    for (l_obj = frame_meta->obj_meta_list; l_obj != NULL;
-         l_obj = l_obj->next)
+    for (l_obj = frame_meta->obj_meta_list; l_obj != NULL; l_obj = l_obj->next)
     {
-      obj_meta = (NvDsObjectMeta *)(l_obj->data);
+      NvDsObjectMeta *obj_meta = (NvDsObjectMeta *)(l_obj->data);
       if (obj_meta->class_id == PGIE_CLASS_ID_VEHICLE)
       {
         vehicle_count++;
@@ -84,42 +73,17 @@ osd_sink_pad_buffer_probe(GstPad *pad, GstPadProbeInfo *info,
         num_rects++;
       }
     }
-    display_meta = nvds_acquire_display_meta_from_pool(batch_meta);
-    NvOSD_TextParams *txt_params = &display_meta->text_params[0];
-    display_meta->num_labels = 1;
-    txt_params->display_text = g_malloc0(MAX_DISPLAY_LEN);
-    offset = snprintf(txt_params->display_text, MAX_DISPLAY_LEN, "Person = %d ", person_count);
-    offset = snprintf(txt_params->display_text + offset, MAX_DISPLAY_LEN, "Vehicle = %d ", vehicle_count);
-
-    /* Now set the offsets where the string should appear */
-    txt_params->x_offset = 10;
-    txt_params->y_offset = 12;
-
-    /* Font , font-color and font-size */
-    txt_params->font_params.font_name = "Serif";
-    txt_params->font_params.font_size = 10;
-    txt_params->font_params.font_color.red = 1.0;
-    txt_params->font_params.font_color.green = 1.0;
-    txt_params->font_params.font_color.blue = 1.0;
-    txt_params->font_params.font_color.alpha = 1.0;
-
-    /* Text background color */
-    txt_params->set_bg_clr = 1;
-    txt_params->text_bg_clr.red = 0.0;
-    txt_params->text_bg_clr.green = 0.0;
-    txt_params->text_bg_clr.blue = 0.0;
-    txt_params->text_bg_clr.alpha = 1.0;
-
-    nvds_add_display_meta_to_frame(frame_meta, display_meta);
   }
 
   g_print("Frame Number = %d Number of objects = %d "
           "Vehicle Count = %d Person Count = %d\n",
           frame_number, num_rects, vehicle_count, person_count);
+
   frame_number++;
   return GST_PAD_PROBE_OK;
 }
 
+/* Handle GstBus messages */
 static gboolean
 bus_call(GstBus *bus, GstMessage *msg, gpointer data)
 {
@@ -153,16 +117,23 @@ bus_call(GstBus *bus, GstMessage *msg, gpointer data)
 int main(int argc, char *argv[])
 {
   GMainLoop *loop = NULL;
+
+  /* GStreamer elements */
   GstElement *pipeline = NULL, *source = NULL, *h264parser = NULL,
-             *decoder = NULL, *streammux = NULL, *sink = NULL, *pgie = NULL, 
-             *nvvidconv = NULL, *nvosd = NULL, *enc = NULL, *filesink = NULL;
+             *decoder = NULL, *streammux = NULL, *pgie = NULL,
+             *nvvidconv = NULL, *nvosd = NULL;
+
+  /* For MP4 output */
+  GstElement *enc = NULL, *h264parser_out = NULL, *mux = NULL, *filesink = NULL;
 
   GstBus *bus = NULL;
   guint bus_watch_id;
   GstPad *osd_sink_pad = NULL;
+
   gboolean yaml_config = FALSE;
   NvDsGieType pgie_type = NVDS_GIE_PLUGIN_INFER;
 
+  /* CUDA device info */
   int current_device = -1;
   cudaGetDevice(&current_device);
   struct cudaDeviceProp prop;
@@ -171,88 +142,175 @@ int main(int argc, char *argv[])
   /* Check input arguments */
   if (argc != 2)
   {
-    g_printerr("Usage: %s <yml file>\n", argv[0]);
-    g_printerr("OR: %s <H264 filename>\n", argv[0]);
+    g_printerr("Usage: %s <H264 file or .yml/.yaml>\n", argv[0]);
     return -1;
   }
 
-  /* Standard GStreamer initialization */
+  /* Initialize GStreamer */
   gst_init(&argc, &argv);
   loop = g_main_loop_new(NULL, FALSE);
 
-  /* Parse inference plugin type */
+  /* Check if we're using a YML config */
   yaml_config = (g_str_has_suffix(argv[1], ".yml") ||
                  g_str_has_suffix(argv[1], ".yaml"));
 
   if (yaml_config)
   {
-    RETURN_ON_PARSER_ERROR(nvds_parse_gie_type(&pgie_type, argv[1],
-                                               "primary-gie"));
+    RETURN_ON_PARSER_ERROR(nvds_parse_gie_type(&pgie_type, argv[1], "primary-gie"));
   }
 
   /* Create gstreamer elements */
   pipeline = gst_pipeline_new("dstest1-pipeline");
 
-  /* Source element for reading from the file */
+  /* Source: reading from file (H264 elementary stream) */
   source = gst_element_factory_make("filesrc", "file-source");
   h264parser = gst_element_factory_make("h264parse", "h264-parser");
   decoder = gst_element_factory_make("nvv4l2decoder", "nvv4l2-decoder");
+
+  /* streammux: batch multiple streams (here we use only 1) */
   streammux = gst_element_factory_make("nvstreammux", "stream-muxer");
+
+  /* Primary inference (PGIE) */
+  /* (If nvinferserver is needed, check pgie_type) */
   pgie = gst_element_factory_make("nvinfer", "primary-nvinference-engine");
+
+  /* Convert NV12->RGBA for OSD */
   nvvidconv = gst_element_factory_make("nvvideoconvert", "nvvideo-converter");
+
+  /* OSD to draw bounding boxes etc. */
   nvosd = gst_element_factory_make("nvdsosd", "nv-onscreendisplay");
 
-  /* New elements for saving the output */
+  /* For MP4 output: H.264 encoder -> parser -> MP4 mux -> filesink */
   enc = gst_element_factory_make("nvv4l2h264enc", "h264-encoder");
+  h264parser_out = gst_element_factory_make("h264parse", "h264-parser-out");
+  mux = gst_element_factory_make("qtmux", "qtmux");
   filesink = gst_element_factory_make("filesink", "file-sink");
 
-  /* Check if elements are created successfully */
-  if (!pipeline || !source || !h264parser || !decoder || !streammux || 
-      !pgie || !nvvidconv || !nvosd || !enc || !filesink)
+  if (!pipeline || !source || !h264parser || !decoder || !streammux ||
+      !pgie || !nvvidconv || !nvosd || !enc || !h264parser_out || !mux || !filesink)
   {
-    g_printerr("One element could not be created. Exiting.\n");
+    g_printerr("Failed to create one or more GStreamer elements.\n");
     return -1;
   }
 
   /* Set element properties */
   g_object_set(G_OBJECT(source), "location", argv[1], NULL);
-  g_object_set(G_OBJECT(streammux), "batch-size", 1, "width", MUXER_OUTPUT_WIDTH,
-               "height", MUXER_OUTPUT_HEIGHT, "batched-push-timeout", MUXER_BATCH_TIMEOUT_USEC, NULL);
-  g_object_set(G_OBJECT(pgie), "config-file-path", "dstest1_pgie_config.txt", NULL);
-  g_object_set(G_OBJECT(filesink), "location", "output.mp4", NULL);
 
-  /* Add elements to the pipeline */
+  /* streammux properties */
+  g_object_set(G_OBJECT(streammux), "batch-size", 1, 
+               "width", MUXER_OUTPUT_WIDTH,
+               "height", MUXER_OUTPUT_HEIGHT,
+               "batched-push-timeout", MUXER_BATCH_TIMEOUT_USEC, 
+               NULL);
+
+  /* pgie config file (if needed) */
+  if (!yaml_config)
+  {
+    /* 예시: txt config 사용 */
+    g_object_set(G_OBJECT(pgie), 
+      "config-file-path", "dstest1_pgie_config.txt", 
+      NULL);
+  }
+  else
+  {
+    /* YAML config 파싱 */
+    RETURN_ON_PARSER_ERROR(nvds_parse_streammux(streammux, argv[1], "streammux"));
+    RETURN_ON_PARSER_ERROR(nvds_parse_gie(pgie, argv[1], "primary-gie"));
+  }
+
+  /* filesink for final output */
+  g_object_set(G_OBJECT(filesink), 
+               "location", "output.mp4", 
+               /* 파일 저장 시에는 sync, async를 끄는 경우 많음 */
+               "sync", FALSE,
+               "async", FALSE,
+               NULL);
+
+  /* Optional: set bitrate on encoder, e.g. 4Mbps */
+  g_object_set(G_OBJECT(enc), "bitrate", 4000000, NULL);
+
+  /* Add elements to pipeline */
   gst_bin_add_many(GST_BIN(pipeline),
-                   source, h264parser, decoder, streammux, pgie,
-                   nvvidconv, nvosd, enc, filesink, NULL);
+                   source, h264parser, decoder,
+                   streammux, pgie, nvvidconv, nvosd,
+                   enc, h264parser_out, mux, filesink,
+                   NULL);
 
-  /* Link elements together */
+  /* Bus and watch */
+  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+  bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
+  gst_object_unref(bus);
+
+  /* ---- Link elements ---- */
+
+  /* 1) source -> h264parser -> decoder */
   if (!gst_element_link_many(source, h264parser, decoder, NULL))
   {
-    g_printerr("Elements could not be linked: source to decoder. Exiting.\n");
+    g_printerr("Could not link source->parser->decoder\n");
     return -1;
   }
 
-  if (!gst_element_link_many(streammux, pgie, nvvidconv, nvosd, enc, filesink, NULL))
+  /* 2) decoder src pad -> streammux sink pad */
   {
-    g_printerr("Elements could not be linked: streammux to filesink. Exiting.\n");
+    GstPad *decoder_src_pad = gst_element_get_static_pad(decoder, "src");
+    GstPad *streammux_sink_pad = gst_element_request_pad_simple(streammux, "sink_0");
+    if (!decoder_src_pad || !streammux_sink_pad)
+    {
+      g_printerr("Failed to get pads from decoder/streammux\n");
+      return -1;
+    }
+    if (gst_pad_link(decoder_src_pad, streammux_sink_pad) != GST_PAD_LINK_OK)
+    {
+      g_printerr("Failed to link decoder src to streammux sink\n");
+      gst_object_unref(decoder_src_pad);
+      gst_object_unref(streammux_sink_pad);
+      return -1;
+    }
+    gst_object_unref(decoder_src_pad);
+    gst_object_unref(streammux_sink_pad);
+  }
+
+  /* 3) streammux -> pgie -> nvvidconv -> nvosd */
+  if (!gst_element_link_many(streammux, pgie, nvvidconv, nvosd, NULL))
+  {
+    g_printerr("Could not link streammux->pgie->nvvidconv->nvosd\n");
     return -1;
   }
 
-  /* Set the pipeline to "playing" state */
+  /* OSD에서 메타 정보 확인 (probe) */
+  osd_sink_pad = gst_element_get_static_pad(nvosd, "sink");
+  if (!osd_sink_pad)
+  {
+    g_printerr("Unable to get sink pad from nvosd\n");
+  }
+  else
+  {
+    gst_pad_add_probe(osd_sink_pad, GST_PAD_PROBE_TYPE_BUFFER,
+                      osd_sink_pad_buffer_probe, NULL, NULL);
+    gst_object_unref(osd_sink_pad);
+  }
+
+  /* 4) nvosd -> encoder -> h264parse_out -> qtmux -> filesink */
+  if (!gst_element_link_many(nvosd, enc, h264parser_out, mux, filesink, NULL))
+  {
+    g_printerr("Could not link nvosd->enc->parser_out->mux->filesink\n");
+    return -1;
+  }
+
+  /* Set pipeline to playing */
   g_print("Using file: %s\n", argv[1]);
   gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-  /* Wait till pipeline encounters an error or EOS */
   g_print("Running...\n");
   g_main_loop_run(loop);
 
-  /* Clean up */
+  /* Cleanup */
   g_print("Returned, stopping playback\n");
   gst_element_set_state(pipeline, GST_STATE_NULL);
   g_print("Deleting pipeline\n");
   gst_object_unref(GST_OBJECT(pipeline));
   g_source_remove(bus_watch_id);
   g_main_loop_unref(loop);
+
   return 0;
 }
